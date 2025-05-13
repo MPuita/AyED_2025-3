dffa76e1fdeafb345d58152a97bdd24c00655e9c
MPuita <matiasnpc123@gmail.com>
2025-05-13 08:51:21 -0300

    Add files via upload

---
+++ 3_Puita1_M/1_PC3_1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int celsius, fahrenheit;
+            Console.WriteLine("Ingrese un temperatura en Celsius");
+            celsius = Convert.ToInt32(Console.ReadLine());
+            fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine(celsius + "°C a fahrenheit: " + fahrenheit + "°F");
+            Console.ReadKey();
+        }
+    }
+}
