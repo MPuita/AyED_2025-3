a1daa6b2ee7a667da029cd2d272abc6208485b2e
MPuita <matiasnpc123@gmail.com>
2025-05-15 09:12:29 -0300

    Add files via upload

---
+++ 3_Puita1_M/1_PC3_3/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int año, an1, an2;
+            Console.WriteLine("Ingrese un año para determinar si es bisiesto o no:");
+            año = Convert.ToInt32(Console.ReadLine());
+            an1 = año % 4;
+            an2 = año % 400;
+            if (an1 == 0 || an2 == 0)
+            {
+                Console.WriteLine("El año es bisiesto");
+            }
+            else
+            {
+                Console.WriteLine("El año es bisiesto");
+            }
+            Console.ReadKey();
+        }
+    }
+}
