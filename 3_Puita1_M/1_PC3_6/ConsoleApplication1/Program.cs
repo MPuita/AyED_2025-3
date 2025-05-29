16b9912b3aea193f802ad2c2cc59cfc5e1c93ba1
MPuita <matiasnpc123@gmail.com>
2025-05-29 10:45:17 -0300

    Add files via upload

---
+++ 3_Puita1_M/1_PC3_6/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cilindros, pistas, sectores;
+            Console.WriteLine("Ingrese la cantidad de cilindros");
+            cilindros = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de pistas");
+            pistas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese la cantidad de sectores");
+            sectores = Convert.ToInt32(Console.ReadLine());
+            long capacidadBytes = (long)cilindros * pistas * sectores * 512;
+            double capacidadKB = (double)capacidadBytes / 1024;
+            double capacidadMB = capacidadKB / 1024;
+            double capacidadGB = capacidadMB / 1024; Console.WriteLine("Capacidad del disco duro en: kilobytes " + capacidadKB + " || megabytes " + capacidadMB + " || gigabytes " + capacidadGB);
+            Console.ReadKey();
+        }
+    }
+}
